7c1b897744169d88327648dd22d503538f81bf02
Laura <kocetkova@beeble.com>
2022-06-16 22:33:01 +0300

    git init

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  reactStrictMode: true,
+    i18n: {
+       // providing the locales supported by your application
+      locales: ["en-US", "es-ES", "it-IT", 'lv'],
+      //  default locale used when the non-locale paths are visited
+      defaultLocale: "en-US",
+    },
+}
